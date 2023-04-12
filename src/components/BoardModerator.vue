10a4b5de72ed32fe141b2533888fc5476c6f15e9
inesamzr <ines.amzert@etu.umontpellier.fr>
2023-04-12 13:32:32 +0200

    authentification

---
+++ src/components/BoardModerator.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>{{ content }}</h3>
+    </header>
+  </div>
+</template>
+
+<script>
+import UserService from "../services/user.service";
+/* TO DO: faudra changer ce code pour que ça corresponde au moderateur */
+
+export default {
+  name: "BoardModerator",
+  data() {
+    return {
+      content: "",
+    };
+  },
+  mounted() {
+    UserService.getUserBoard().then(
+      (response) => {
+        this.content = response.data;
+      },
+      (error) => {
+        this.content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  },
+};
+</script>

230728efb3673a9bb3947434bd8c79d6d390a7da
inesamzr <ines.amzert@etu.umontpellier.fr>
2023-05-29 15:54:40 +0200

    bug dans la bd

--- src/configs/api.config.js
+++ src/configs/api.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    apiUrl : "https://alesio-api.onrender.com/api"
+    //apiUrl : "https://alesio-api.onrender.com/api"
+    apiUrl : "https://alesio-api-av8x.onrender.com/api"
 }
